d5ecfab656c00ef36470f028312bd2fb404a76c7
Vũ Quốc Toàn <90686311+toandz22@users.noreply.github.com>
2023-04-28 09:31:39 +0700

    Delete level5 directory

--- level5/5.6.php
+++
@@ -1,9 +0,0 @@
-5.6 <br>
-<?php
-$arr = ("i love         tech        lead");
-function xoakhoangtrang($arr){
-    $t = trim($arr);
-    return $t;
-}
-echo "chuỗi sau khi xóa khoảng trắng là : ".xoakhoangtrang($arr);
-?>
